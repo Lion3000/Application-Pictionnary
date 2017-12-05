aea9e6c81eda6be54dd18d7390c38a23b19247d0
Alex zarzitski <alex.zarzitski@gmail.com>
2017-12-05 14:49:21 +0100

    Ajustement du css pour le screen

--- inscription.php
+++ inscription.php
@@ -13,16 +13,16 @@
 			<ul>  
 				</li>  
 				<li>  
-					<label for="email">* E-mail :</label>  
+					<label for="email">E-mail :</label>  
 					<input type="email" name="email" id="email" autofocus required />            
 					<span class="form_hint">Format attendu "name@something.com"</span>  
 				</li>  
 				<li>  
-					<label for="prenom">* Prénom :</label>  
+					<label for="prenom">Prénom :</label>  
 					<input type="text" name="prenom" id="prenom" placeholder="votre prénom" required />  
 				</li>
 				<li>  
-					<label for="nom">* Nom :</label>  
+					<label for="nom">Nom :</label>  
 					<input type="text" name="nom" id="nom" required />  
 				</li>
 				<li>  
@@ -34,35 +34,35 @@
 					<input type="url" name="siteWeb" id="siteWeb" />  
 				</li>
 				<li>  
-					<label for="sexe">Sexe :</label> 
+					<label for="sexe">Sexe :</label><br />
 					<ul>  
 						<li><input type="radio" name="sexe" value="female" id="genre" > Female</li>
 						<li><input type="radio" name="sexe" value="homme" id="genre" > Homme</li>
 					</ul>
 				</li>
 				<li>  
-					<label for="birthdate">* Date de naissance:</label>  
+					<label for="birthdate">Date de naissance :</label>  
 					<input type="date" name="birthdate" id="birthdate" placeholder="JJ/MM/AAAA" required onchange="computeAge()"/>  
 					<span class="form_hint">Format attendu "JJ/MM/AAAA"</span>  
 				</li>  
 				<li>  
-					<label for="age">Age:</label>  
+					<label for="age">Age :</label>  
 					<input type="number" name="age" id="age" disabled/>               
 				</li>  
 				<li>  
-					<label for="ville">ville :</label>  
+					<label for="ville">Ville :</label>  
 					<input type="text" name="ville" id="ville" />  
 				</li>
 				<li>  
-					<label for="taille">taille en mètre:</label>  
+					<label for="taille">Taille en mètre :</label>  
 					<input type="range" name="taille" id="taille" min="0" max="2.5" step="0.1"/>  
 				</li>
 				<li>  
-					<label for="couleur">couleur préférée :</label>  
+					<label for="couleur">Couleur préférée :</label>  
 					<input type="color" value="black" name="couleur" id="couleur" />  
 				</li>
 				<li>  
-					<label for="profilepicfile">Photo de profil:</label>  
+					<label for="profilepicfile">Photo de profil :</label>  
 					<input type="file" id="profilepicfile" onchange="loadProfilePic(this)"/>  
 					<span class="form_hint">Choisissez une image.</span>  
 					<input type="hidden" name="profilepic" id="profilepic"/>  
@@ -74,18 +74,18 @@
 					de la redimensionner, et de l'enregistrer sous forme d'une data url-->  
 				</li>  
 				<li>
-					<label for="mdp1">* Mot de passe :</label>  
+					<label for="mdp1">Mot de passe :</label>  
 					<input type="password" name="password" id="mdp1" pattern="\w{6,8}" onkeyup="validateMdp2()" placeholder = "votre mot de passe" title = "Le mot de passe doit contenir de 6 à 8 caractères alphanumériques." required>  
 				   
 					 <span class="form_hint">De 6 à 8 caractères alphanumériques.</span>  
 				</li>  
 				<li>  
-					<label for="mdp2">* Confirmez mot de passe :</label>  
+					<label for="mdp2">Confirmez mot de passe :</label>  
 					<input type="password" id="mdp2" required onkeyup="validateMdp2()" placeholder="retapez le mot de passe" required>   
 					<span class="form_hint">Les mots de passes doivent être égaux.</span>  
 				</li> 
 				<li>  
-					<input type="submit" value="Soumettre Formulaire">  
+					<input id="submit" type="submit" value="Soumettre Formulaire">  
 				</li>  
 			</ul>  
 		</form>  
