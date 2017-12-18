d293a8e2fe328088b252f2e7b3a774e154f0d0ac
Alex zarzitski <alex.zarzitski@gmail.com>
2017-12-18 11:02:20 +0100

    fin codage header.php

--- header.php
+++ header.php
@@ -1,3 +1,31 @@
-<header>
-
+<header class="w3-card-4 w3-margin">
+<?PHP
+session_start();
+if(isset($_SESSION['user'])){
+	$user = unserialize($_SESSION['user']);
+?>
+	<div>
+		<h2>Bonjour <?PHP echo $user['nom'] . " " . $user['prenom']; ?></h2>
+		<img src="<?PHP echo $user['profilepic']; ?>" />
+	</div>
+	<nav class="w3-container">
+		<a href="logout.php" class="w3-btn w3-blue" >Déconnexion</a>
+	</nav>
+<?PHP
+}
+else{
+?>
+	<nav class="w3-container">
+		<form method="post" action="req_login.php">
+			<label>Login</label>
+			<input class="w3-input" type="text">
+			<label>Pot de passe</label>
+			<input class="w3-input" type="password">
+			<button class="w3-btn w3-blue">Connexion</button>
+		</form>
+		<a href="inscription.php" class="w3-btn w3-blue" >Inscription</a>
+	</nav>
+<?PHP
+}
+?>
 </header>
